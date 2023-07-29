8a53f45d5a83da3e984a7bb833b0a3e99121a639
Manalan <manalananand@gmail.com>
2023-07-29 20:55:10 +1000

    cleaned up random()

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from discord.ext import commands
 from apitokens import *
 
 import requests
-import random
+import random as rd
 import os
 
+from additional_functions import *
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,27 +35,21 @@ async def hello(ctx):
 async def random(ctx):
     "Pulls a random XKCD comic by using >random."
     
+    current_comic_num = getLatestComicNumber()
 
-    current_comic_url = "https://xkcd.com/info.0.json"
+    random_comic_num = rd.randint(1,current_comic_num)
 
-    response = requests.get(current_comic_url).json()
-    current_comic_num = response['num']
-    # print(type(current_comic_num))
-    random_comic_number = random.randint(1,current_comic_num)
-    #print(f'comic num:{random_comic_number}')
-    random_xkcd_url = 'https://xkcd.com/' + str(random_comic_number) + '/info.0.json'
-    random_comic_response = requests.get(random_xkcd_url).json()
-    random_comic_url = random_comic_response['img']
-    random_comic_title = random_comic_response['safe_title']
-    random_comic_png = requests.get(random_comic_url)
+    random_comic_url = 'https://xkcd.com/' + str(random_comic_num) + '/info.0.json'
+    comic,title = pullComic(random_comic_url)
+
     if not os.path.exists("./XKCD"):
         os.makedirs("./XKCD")
     path = "./XKCD/XKCD.jpg"
         
     with open(path,'wb') as image:
-        image.write(random_comic_png.content)
+        image.write(comic.content)
     await ctx.send('There you go!')
-    await ctx.send(f'Title: `{random_comic_title}`')
+    await ctx.send(f'Title: `{title}`')
     await ctx.send(file = discord.File(path))
 
 
