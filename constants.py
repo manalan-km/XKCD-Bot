41ccbe12a3a380f0182c7211f5a100492aa9ea2b
Manalan <manalananand@gmail.com>
2023-08-03 17:24:33 +1000

    init constants for emojis

---
+++ constants.py
@@ -0,0 +1,5 @@
+class Constants:
+    def __init__(self):
+        self.right_arrow_emoji = '➡️' #emoji for right arrow
+        self.left_arrow_emoji = '⬅️'  #emoji for left arrow
+        self.random_emoji = '🔀'      #emoji for random 
